d68f6d47d6c8df40d3cf5b2ad6d4e27b8aee0e6f
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-05-12 17:11:22 +0100

    Implemented connection to database

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-#Create a Flask app named after this file
+#Create a Flask app named after this file, then set up the DB connection
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://todo:$PASSWORDGOESHERE@localhost:5432/todoapp'
+db = SQLAlchemy(app)
+
+#Create the model, then instantiate it
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
+
+    def __repr__(self):
+        return f'<Todo #{self.id}: {self.description}>'
+
+db.create_all()
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[{
-        'description': 'Render dummy data',
-        'complete': True
-    }, {
-        'description': 'Set up database',
-        'complete': False
-    }, {
-        'description': 'Set up UX framework',
-        'complete': False
-    }])
+    return render_template('index.html', data=Todo.query.all())
