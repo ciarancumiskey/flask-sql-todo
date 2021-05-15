a33bb63ab1d8bb8f36108f5c3715dda5ae065253
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-05-15 16:00:57 +0100

    Created wrapper classes for lists and clickable links
    Clicking a list's name in the list of lists will now show that list.

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ def index():
 def get_list_todos(list_id):
     return render_template('index.html', 
     lists=TodoList.query.all(),
+    active_list=TodoList.query.get(list_id),
     todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
 
 @app.route('/todos/create', methods=['POST'])
