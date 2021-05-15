17d85ab56d390b7fc5b2d3e2bfedb4f73c1dc013
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-05-15 15:51:21 +0100

    Added a list of the Todo lists
    Each item in the list also has non-functioning "complete all" and "delete" buttons.

--- app.py
+++ app.py
@@ -40,7 +40,8 @@ def index():
 @app.route('/lists/<list_id>')
 def get_list_todos(list_id):
     return render_template('index.html', 
-    data=Todo.query.filter_by(list_id=list_id).order_by('id').all())
+    lists=TodoList.query.all(),
+    todos=Todo.query.filter_by(list_id=list_id).order_by('id').all())
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
